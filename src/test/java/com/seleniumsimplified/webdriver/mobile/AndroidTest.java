24aef8a069dcdc20962c5543cec9e83949b67c91
Rahul Goyal <viacom.rahul@gmail.com>
2014-03-22 08:15:59 +0530

    Initial Commit

---
+++ src/test/java/com/seleniumsimplified/webdriver/mobile/AndroidTest.java
@@ -0,0 +1,22 @@
+package com.seleniumsimplified.webdriver.mobile;
+
+//import org.openqa.selenium.android.AndroidDriver;
+
+public class AndroidTest {
+
+    // after installing the android server for webdriver
+    // and finding the ip address from settings
+    //@Ignore("setup with the IP of your device")
+/*    @Test
+    public void BasicAndroidTest() throws MalformedURLException {
+        WebDriver driver = new AndroidDriver("http://192.168.1.165:8080/wd/hub");
+        driver.get("http://seleniumsimplified.com");
+        driver.quit();
+    }
+*/
+
+    // Android driver is no longer present in the main WebDriver code base
+    // The official documentation recommends the use of Selendroid instead
+    //https://code.google.com/p/selenium/wiki/AndroidDriver#Use_instead.
+    // http://selendroid.io/mobileWeb.html
+}
